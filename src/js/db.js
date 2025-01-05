18dc737714a5666eaf234ed32a5d941b2de09c3e
César García <cesar.garciamcs@outlook.com>
2025-01-05 17:22:27 -0600

    terminando de agregar la informacion al objeto

--- src/js/db.js
+++ src/js/db.js
@@ -3,7 +3,6 @@ const elementos = [
         titulo: 'Desktop',
         seccion: [
             {
-                id: 1,
                 link: 'https://acortar.link/YxZKSg',
                 nombre: 'LAGKAPTEN / ALEX',
                 avif: '/src/img/lagkapten.avif',
@@ -17,7 +16,6 @@ const elementos = [
         titulo: 'Keyboard & Mouse',
         seccion: [
             {
-                id: 1,
                 link: 'https://acortar.link/lP69KF',
                 nombre: 'Logitech MX Keys',
                 avif: '/src/img/logitechkeys.avif',
@@ -26,7 +24,6 @@ const elementos = [
                 desc: 'Teclado inalámbrico, bajo Perfil, preciso y silencioso, Teclas programables, retroiluminado, Bluetooth, USB C Recargable, para Windows PC, Linux, Chrome, Mac-Negro'
             },
             {
-                id: 2,
                 link: 'https://acortar.link/XsDoQF',
                 nombre: 'Logitech MX Keys Mini',
                 avif: '/src/img/logitechmini.avif',
@@ -35,13 +32,244 @@ const elementos = [
                 desc: 'Teclado Minimalista inalámbrico Iluminado Compacto, Bluetooth, retroiluminado, USB-C, Compatible con Apple macOS, iOS, Windows, Linux, Android, Metal Build'
             },
             {
-                id: 3,
                 link: 'https://acortar.link/MVKTB5',
                 nombre: 'Logitech MX Master 3s',
                 avif: '/src/img/logitechmaster.avif',
                 webp: '/src/img/logitechmaster.webp',
                 jpg: '/src/img/logitechmaster.jpg',
                 desc: 'Mouse Inalámbrico de Desempeño, Scrolling Ultra-rápido, 8K dpi, Seguimiento sobre Vidrio, Clics Silenciosos, USB-C, Bluetooth'
+            }
+        ]
+    },
+    {
+        titulo: 'Laptop / iPad Stands',
+        seccion: [
+            {
+                link: 'https://acortar.link/yCtjrd',
+                nombre: 'Laptop Stand',
+                avif: '/src/img/laptopstand.avif',
+                webp: '/src/img/laptopstand.webp',
+                jpg: '/src/img/laptopstand.jpg',
+                desc: 'Soporte de Aluminio para Laptop, ergonómico, Desmontable, Soporte Elevador para computadora Compatible con Air, Pro, DELL, HP, Lenovo más portátiles de 10 a 15.6 Pulgadas, Color Negro'
+            },
+            {
+                link: 'https://acortar.link/RwxPUD',
+                nombre: 'Laptop Holder',
+                avif: '/src/img/laptopholder.avif',
+                webp: '/src/img/laptopholder.webp',
+                jpg: '/src/img/laptopholder.jpg',
+                desc: 'Soporte Vertical para Laptop para Escritorio, Base Stand Porta Ajustable para Ordenador Portátil, Soporte de Escritorio de Aluminio para MacBook Air Pro, Microsoft Surface Laptop, Negro'
+            },
+            {
+                link: 'https://acortar.link/TomKUa',
+                nombre: 'iPad Stand',
+                avif: '/src/img/ipadstand.avif',
+                webp: '/src/img/ipadstand.webp',
+                jpg: '/src/img/ipadstand.jpg',
+                desc: 'Soporte Delgado con Respaldo Rígido Carcasa con Auto-Sueño/Estela'
+            }
+        ]
+    },
+    {
+        titulo: 'Laptops',
+        seccion: [
+            {
+                link: 'https://acortar.link/T2Cbnr',
+                nombre: 'Macbook Air M2',
+                avif: '/src/img/macbookair.avif',
+                webp: '/src/img/macbookair.webp',
+                jpg: '/src/img/macbookair.jpg',
+                desc: 'Audífonos inalámbricos con Noise Cancelling, Negro'
+            },
+            {
+                link: 'https://a.co/d/fA00Obn',
+                nombre: 'Zenbook OLED 2024',
+                avif: '/src/img/asuszenbook.avif',
+                webp: '/src/img/asuszenbook.webp',
+                jpg: '/src/img/asuszenbook.jpg',
+                desc: 'Audífonos inalámbricos con Noise Cancelling, Negro'
+            }
+        ]
+    },
+    {
+        titulo: 'Audio',
+        seccion: [
+            {
+                link: 'https://acortar.link/Lts0lN',
+                nombre: 'Sony WH-1000XM4',
+                avif: '/src/img/sonym4.avif',
+                webp: '/src/img/sonym4.webp',
+                jpg: '/src/img/sonym4.jpg',
+                desc: 'Audífonos inalámbricos con Noise Cancelling, Negro'
+            },
+            {
+                link: 'https://acortar.link/FY8lhw',
+                nombre: 'Sonos Ace',
+                avif: '/src/img/sonosace.avif',
+                webp: '/src/img/sonosace.webp',
+                jpg: '/src/img/sonosace.jpg',
+                desc: 'Audífonos Inalámbricos Over-Ear con Cancelación Activa de Ruido, Audio Espacial, Audio Lossless y 30 Horas de Batería - Negro'
+            },
+            {
+                link: 'https://acortar.link/wm32w1',
+                nombre: 'KZ ZSN Pro X',
+                avif: '/src/img/zsnpro.avif',
+                webp: '/src/img/zsnpro.webp',
+                jpg: '/src/img/zsnpro.jpg',
+                desc: 'Audífonos intraurales con cable, controlador doble, sonido claro, ergonómicos, ajuste personalizado (S/M/L), audífonos de graves, conector de 3.5 mm'
+            },
+            {
+                link: 'https://acortar.link/CJhYMU',
+                nombre: 'FIFINE',
+                avif: '/src/img/fifine.avif',
+                webp: '/src/img/fifine.webp',
+                jpg: '/src/img/fifine.jpg',
+                desc: 'Micrófono dinámico, XLR/USB Podcast Grabación de PC Micrófono para Voz Sobreviendo, Micrófono de Estudio con Micrófono Silencio, Conector de Auriculares, Control de Volumen de Monitoreo-K688'
+            }
+        ]
+    },
+    {
+        titulo: 'Monitor',
+        seccion: [
+            {
+                link: 'https://a.co/d/9U1ZKGZ',
+                nombre: 'UltraGear 34',
+                avif: '/src/img/lgultrawide.avif',
+                webp: '/src/img/lgultrawide.webp',
+                jpg: '/src/img/lgultrawide.jpg',
+                desc: 'Gaming Monitor 34" VA WQHD 160Hz 1ms MBR AMD FreeSync Premium, HDMI, Display Port, Curvo 1800R'
+            },
+            {
+                link: 'https://a.co/d/7ZVW27M0',
+                nombre: 'GIGABYTE 34',
+                avif: '/src/img/gigabyteultrawide.avif',
+                webp: '/src/img/gigabyteultrawide.webp',
+                jpg: '/src/img/gigabyteultrawide.jpg',
+                desc: 'Monitor Gaming Curvo de 34", QHD, Ultra Wide; 144Hz, Respuesta de 1 ms'
+            },
+            {
+                link: 'https://a.co/d/8hhdJZ7',
+                nombre: 'Odyssey G5',
+                avif: '/src/img/odyssey.avif',
+                webp: '/src/img/odyssey.webp',
+                jpg: '/src/img/odyssey.jpg',
+                desc: 'Monitor gaming con visualización Curva 1000R, 165Hz, 1ms, FreeSync Premium, WQHD'
+            },
+            {
+                link: 'https://acortar.link/s9cpw2',
+                nombre: 'Barra de luz',
+                avif: '/src/img/monitorlight.avif',
+                webp: '/src/img/monitorlight.webp',
+                jpg: '/src/img/monitorlight.jpg',
+                desc: 'Control inalámbrico, fácil instalación, iluminación adicional para computadora sin ocupar espacio en el computadora, no más deslumbramiento'
+            }
+        ]
+    },
+    {
+        titulo: 'Notepads',
+        seccion: [
+            {
+                link: 'https://acortar.link/WP31po',
+                nombre: 'Notepad A4',
+                avif: '/src/img/a4pad.avif',
+                webp: '/src/img/a4pad.webp',
+                jpg: '/src/img/a4pad.jpg',
+                desc: 'Tapete de corte de PVC autocurativo grande de 24 x 36 pulgadas, doble cara, tabla de cortar rotatoria a cuadrícula para manualidades, tela, acolchado, costura, álbumes de recortes, proyecto'
+            },
+            {
+                link: 'https://acortar.link/YWGzhU',
+                nombre: 'Logitech XL',
+                avif: '/src/img/logitechpad.avif',
+                webp: '/src/img/logitechpad.webp',
+                jpg: '/src/img/logitechpad.jpg',
+                desc: 'Mouse Pad Studio Series XL, Multifunción, Mouse Pad Extendido, Protector de Escritorio de Oficina, Base Antideslizante, Resiste a Salpicaduras - Gris'
+            }
+        ]
+    },
+    {
+        titulo: 'WiFi & Connections',
+        seccion: [
+            {
+                link: 'https://acortar.link/zuWyq4',
+                nombre: 'TP-Link',
+                avif: '/src/img/tplink.avif',
+                webp: '/src/img/tplink.webp',
+                jpg: '/src/img/tplink.jpg',
+                desc: 'Deco Sistema WiFi Mesh (o en malla) para todo el hogar: alcanza hasta 867 Mbps, itinerancia sin interrupciones, cobertura de hasta 2800 pies cuadrados'
+            },
+            {
+                link: 'https://acortar.link/VNvfsC',
+                nombre: 'Regleta Escondida',
+                avif: '/src/img/cablemanagment.avif',
+                webp: '/src/img/cablemanagment.webp',
+                jpg: '/src/img/cablemanagment.jpg',
+                desc: 'Redagod Regletas Enchufes con Caja Organizadora de Cables, Regletas de Enchufes con Interruptor'
+            },
+            {
+                link: 'https://acortar.link/2CGc8p',
+                nombre: 'Thunderbolt',
+                avif: '/src/img/thunderbolt.avif',
+                webp: '/src/img/thunderbolt.webp',
+                jpg: '/src/img/thunderbolt.jpg',
+                desc: 'Estación de Acoplamiento 13 en 1 de 40 Gbps con Pantalla Dual 4K@60Hz o Única 8K, Carga 90 W para Laptop, 2.5 GbE, SD/TF 4.0 para Laptops MacBook Windows PC'
+            }
+        ]
+    },
+    {
+        titulo: 'Chairs',
+        seccion: [
+            {
+                link: 'https://acortar.link/vWei0n',
+                nombre: 'ELABEST',
+                avif: '/src/img/elabest.avif',
+                webp: '/src/img/elabest.webp',
+                jpg: '/src/img/elabest.jpg',
+                desc: 'Silla de computadora de malla 3D ajustable, función de inclinación, soporte lumbar ajustable y reposabrazos, asiento de malla ancho cómodo'
+            },
+            {
+                link: 'https://acortar.link/mPyirW',
+                nombre: 'Ofik H1',
+                avif: '/src/img/ofikH1.avif',
+                webp: '/src/img/ofikH1.webp',
+                jpg: '/src/img/ofikH1.jpg',
+                desc: 'Silla De Oficina Sillon Ejecutivo Base Resistente (Blanco) H1'
+            },
+            {
+                link: 'https://acortar.link/EFEbMh',
+                nombre: 'Atlas',
+                avif: '/src/img/atlas.avif',
+                webp: '/src/img/atlas.webp',
+                jpg: '/src/img/atlas.jpg',
+                desc: 'Reposacabezas con suspensión activada para Silla Aeron Herman Miller, Accesorio ergonómico optimizado para Mejorar la Postura'
+            }
+        ]
+    },
+    {
+        titulo: 'Storage',
+        seccion: [
+            {
+                link: 'https://acortar.link/5U57up',
+                nombre: 'LaCie 2TB',
+                avif: '/src/img/lacie.avif',
+                webp: '/src/img/lacie.webp',
+                jpg: '/src/img/lacie.jpg',
+                desc: 'Disco duro - 2 TB - USB 3.0 LAC9000298'
+            },
+            {
+                link: 'https://acortar.link/pRUcjG',
+                nombre: 'UGREEN Carcasa M2',
+                avif: '/src/img/ugreennvme.avif',
+                webp: '/src/img/ugreennvme.webp',
+                jpg: '/src/img/ugreennvme.jpg',
+                desc: 'Disco Duro NVME PCIE Caja 10 Gbps USB C 3.2 Gen 2 con UASP, Caja para SSD M.2 NVME M Key, B y M Key, Compatible con 2230/2242/2260/2280 Cable C to C y A to C 0,25m Gris'
+            },
+            {
+                link: 'https://acortar.link/x6yDvu',
+                nombre: 'WD BLACK 1TB',
+                avif: '/src/img/wdblack.avif',
+                webp: '/src/img/wdblack.webp',
+                jpg: '/src/img/wdblack.jpg',
+                desc: 'Unidad de Estado sólido Interna para Juegos - Gen4 PCIe, M.2 2280, hasta 5,150 MB/s - WDS100T3X0E'
             }
         ]
     }

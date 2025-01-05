7baed4a47f3a4e5f8d97b78dc70793db4125fe76
César García <cesar.garciamcs@outlook.com>
2025-01-05 15:58:10 -0600

    cambiando a css

---
+++ src/js/db.js
@@ -0,0 +1,11 @@
+const productos = [
+    {
+        id: 1,
+        link: 'https://acortar.link/YxZKSg',
+        nombre: 'LAGKAPTEN / ALEX',
+        avif: '/src/img/lagkapten.avif',
+        webp: '/src/img/lagkapten.webp',
+        jpg: '/src/img/lagkapten.jpg',
+        descripcion: 'Un escritorio grande con mucho espacio para poner el ordenador, documentos y todo aquello que quieras tener a mano o guardado en una cajonera.'
+    }
+]
